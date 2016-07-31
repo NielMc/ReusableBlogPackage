ced1b3041a132e84ffe32948dad656fd37cf7c34
Niel <nielmce@gmail.com>
2016-07-31 16:47:54 +0100

    edit url

--- reusable_blog/urls.py
+++ reusable_blog/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^/$', views.post_list, name='post_list'),
+    url(r'^$', views.blog_posts, name='post_list'),
+    url(r'^/$', views.blog_posts, name='post_list'),
     url(r'^(?P<id>\d+)/$', views.post_detail),
     url(r'^post/$', views.new_post, name='new_post'),
 ]
